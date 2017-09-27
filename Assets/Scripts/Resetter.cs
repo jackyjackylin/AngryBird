0a7d30cd59e7c2b33749a79adbb4846e6ffe5d5c
jacky841114 <jacky841114@gmail.com>
2017-09-27 22:34:53 +0800

    練習實作AngryBird遊戲(Final)

---
+++ Assets/Scripts/Resetter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Resetter : MonoBehaviour {
+
+	public Rigidbody2D projectile;
+	public float resetSpeed = 0.025f;
+
+	private float resetSpeedSql;
+	private SpringJoint2D spring;
+
+	void Start () {
+		resetSpeedSql = resetSpeed * resetSpeed;
+		spring = projectile.GetComponent<SpringJoint2D> ();
+	}
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.R)) {
+			Reset ();
+		}
+		if (spring==null &&projectile.velocity.sqrMagnitude < resetSpeedSql) {
+			Reset ();
+		}
+	}
+	void OnTriggerExit2D(Collider2D other){
+		if (other.GetComponent<Rigidbody2D>() == projectile) {
+			Reset ();
+		}
+	}
+
+	void Reset(){
+		Application.LoadLevel (Application.loadedLevel);
+	}
+
+
+}
